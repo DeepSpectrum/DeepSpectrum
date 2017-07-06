ef00faec25f7410c7fe90fe2a9337a9f7bd02d08
Maurice Gerczuk <xmauricex@live.de>
2017-07-06 17:37:40 +0200

    Add CLI feature reduction.

---
+++ custom_arff.py
@@ -0,0 +1,45 @@
+class Reader():
+    def __init__(self, file_object):
+        self.arff_file = file_object
+        self.attributes = []
+        self._parse_header()
+
+    def _parse_header(self):
+        for line in self.arff_file:
+            line = line.strip()
+            if line.lower().startswith('@relation'):
+                self.relation = ' '.join(line.split(' ')[1:])
+            if line.lower().startswith('@attribute'):
+                self.attributes.append((line.split(' ')[1], line.split(' ')[2]))
+            if line.lower().startswith('@data'):
+                break
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = next(self.arff_file).strip().split(',')
+        if len(line) == len(self.attributes):
+            return line
+        else:
+            return self.__next__()
+
+
+class Writer():
+    def __init__(self, file_object, relation_name, attributes):
+        self.arff_file = file_object
+        self.relation_name = relation_name
+        self.attributes = attributes
+        self._write_header()
+
+    def _write_header(self):
+        self.arff_file.write(' '.join(['@relation', '\'{}\''.format(self.relation_name)+'\n']))
+        self.arff_file.write('\n')
+        for attribute_name, attribute_type in self.attributes:
+            self.arff_file.write(' '.join(['@attribute', attribute_name, attribute_type]) + '\n')
+        self.arff_file.write('\n')
+        self.arff_file.write('@data\n')
+
+    def writerow(self, row):
+        self.arff_file.write(','.join(row)+'\n')
+
--- feature_reduction.py
+++ feature_reduction.py
@@ -1,9 +1,14 @@
+import argparse
+import csv
+from os import makedirs
+from os.path import dirname, splitext, abspath
+
 import arff
 import numpy as np
 import pandas as pd
-from os import makedirs
-from os.path import dirname
 from tqdm import tqdm
+
+import custom_arff
 
 
 def _load_features(feature_file):
@@ -66,7 +71,7 @@ def _write_arff(features, output, arff_header, arff_relation):
     :return: Nothing
     """
     arff_data = dict()
-    arff_data['relation'] = arff_relation+" reduced"
+    arff_data['relation'] = arff_relation + " reduced"
     arff_data['attributes'] = [attribute for attribute in arff_header if attribute[0] in features.columns]
     arff_data['data'] = features.values
     arff.dump(arff_data, open(output, 'w'))
@@ -82,3 +87,67 @@ def _write_csv(features, output):
     features.to_csv(output)
 
 
+def _reduce(file_name):
+    with open(file_name, newline='') as file:
+        if file_name.endswith('.arff'):
+            reader = custom_arff.Reader(file)
+        else:
+            reader = csv.reader(file, delimiter=',')
+            next(reader)
+
+        first_row = np.array(next(reader))
+        indices_to_remove = set(np.where((first_row == '0.0') | (first_row == '0'))[0])
+        for line in reader:
+            data = np.array(line)
+            indices_to_remove = indices_to_remove.intersection(np.where((data == '0.0') | (data == '0'))[0])
+    return indices_to_remove
+
+
+def _apply_reduction(input, output, indices_to_remove):
+    with open(input, newline='') as input_file:
+        with open(output, 'w', newline='') as output_file:
+            input_arff = input.endswith('.arff')
+            output_arff = output.endswith('.arff')
+            if input_arff:
+                reader = custom_arff.Reader(input_file)
+                reduced_attributes = [reader.attributes[x] for x in range(len(reader.attributes)) if
+                                      x not in indices_to_remove]
+                if output_arff:
+                    writer = custom_arff.Writer(output_file, '\'Reduced ' + reader.relation[1:], reduced_attributes)
+                else:
+                    writer = csv.writer(output_file, delimiter=',')
+                    writer.writerow([attribute[0] for attribute in reduced_attributes])
+
+            else:
+                reader = csv.reader(input_file, delimiter=',')
+                attributes = next(reader)
+                reduced_attributes = [attributes[x] for x in range(len(attributes)) if x not in indices_to_remove]
+                if output_arff:
+                    arff_attributes = [(attribute_name, 'numeric') for attribute_name in reduced_attributes]
+                    arff_attributes[0][1] = 'string'
+                    arff_attributes[:-1][1] = 'nominal'
+                    writer = custom_arff.Writer(output_file, '\'Reduced Features\'', arff_attributes)
+                else:
+                    writer = csv.writer(output_file, delimiter=',')
+                    writer.writerow(reduced_attributes)
+
+            for line in reader:
+                reduced_line = [line[index] for index in range(len(line)) if index not in indices_to_remove]
+                writer.writerow(reduced_line)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Reduce a list of feature files by removing features that are always zero in the first given file.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('input', nargs='+', help='The feature files to reduce.')
+    args = vars(parser.parse_args())
+    input_files = list(map(abspath, args['input']))
+    print('Selecting features to remove from {}...'.format(input_files[0]))
+    features_to_remove = _reduce(input_files[0])
+    print('Removing {} features at the following indices: {}'.format(
+        str(len(features_to_remove)), ','.join(list(map(str, sorted(features_to_remove))))))
+    for input_file in input_files:
+        output_file = splitext(input_file)[0] + '.reduced' + splitext(input_file)[1]
+        print('Reducing {}...'.format(input_file))
+        _apply_reduction(input_file, output_file, features_to_remove)
