5d92246b7db3364d71fcbe35247060ffaa801b65
Maurice Gerczuk <mauricegerc@gmail.com>
2019-05-15 11:42:41 +0200

    Add support for ogg and flac.
    Also update the tests accordingly.

--- src/deepspectrum/cli/configuration.py
+++ src/deepspectrum/cli/configuration.py
@@ -4,6 +4,7 @@ import configparser
 import fnmatch
 import re
 import decimal
+from enum import Enum
 from multiprocessing import cpu_count
 from os import makedirs, walk
 from matplotlib import cm
@@ -29,23 +30,33 @@ def _check_positive(ctx, param, value):
     return ivalue
 
 
+class Filetypes(Enum):
+    AUDIO = ['wav', 'ogg', 'flac', 'mp3']
+    IMAGE = ['png', 'jpg']
+
+
 GENERAL_OPTIONS = [
     click.argument(
         "input",
-        type=click.Path(dir_okay=True, file_okay=True, exists=True, readable=True),
+        type=click.Path(dir_okay=True,
+                        file_okay=True,
+                        exists=True,
+                        readable=True),
     ),
     click.option(
         "-c",
         "--config",
         type=click.Path(readable=True, dir_okay=False),
-        help="Path to configuration file which specifies caffe model and weight files. If this file does not exist a new one is created and filled with the standard settings.",
+        help=
+        "Path to configuration file which specifies caffe model and weight files. If this file does not exist a new one is created and filled with the standard settings.",
         default=join(dirname(realpath(__file__)), "deep.conf"),
     ),
     click.option(
         "-np",
         "--number-of-processes",
         type=click.IntRange(1, max_np, clamp=True),
-        help="Define the number of processes used in parallel for the extraction. If None defaults to cpu-count",
+        help=
+        "Define the number of processes used in parallel for the extraction. If None defaults to cpu-count",
         default=max_np,
     ),
 ]
@@ -54,21 +65,24 @@ PLOTTING_OPTIONS = [
     click.option(
         "-s",
         "--start",
-        help="Set a start time from which features should be extracted from the audio files.",
+        help=
+        "Set a start time from which features should be extracted from the audio files.",
         type=decimal.Decimal,
         default=0,
     ),
     click.option(
         "-e",
         "--end",
-        help="Set a end time until which features should be extracted from the audio files.",
+        help=
+        "Set a end time until which features should be extracted from the audio files.",
         type=decimal.Decimal,
         default=None,
     ),
     click.option(
         "-t",
         "--window-size-and-hop",
-        help="Extract deep spectrum features from windows with specified length and hopsize in seconds.",
+        help=
+        "Extract deep spectrum features from windows with specified length and hopsize in seconds.",
         nargs=2,
         type=decimal.Decimal,
         default=[None, None],
@@ -84,33 +98,36 @@ PLOTTING_OPTIONS = [
         "--colour-map",
         default="viridis",
         help="define the matplotlib colour map to use for the spectrograms",
-        type=click.Choice([m for m in cm.cmap_d])
-    ),  # ,
+        type=click.Choice([m for m in cm.cmap_d])),  # ,
     # choices=sorted([m for m in cm.cmap_d]))
     click.option(
         "-fql",
         "--frequency-limit",
         type=int,
-        help="define a limit for the frequency axis for plotting the spectrograms",
+        help=
+        "define a limit for the frequency axis for plotting the spectrograms",
         default=None,
     ),
     click.option(
         "-sr",
         "--sample-rate",
         type=int,
-        help="define a target sample rate for reading the audio files. Audio files will be resampled to this rate before spectrograms are extracted.",
+        help=
+        "define a target sample rate for reading the audio files. Audio files will be resampled to this rate before spectrograms are extracted.",
         default=None,
     ),
     click.option(
         "-so",
         "--spectrogram-out",
-        help="define an existing folder where spectrogram plots should be saved during feature extraction. By default, spectrograms are not saved on disk to speed up extraction.",
+        help=
+        "define an existing folder where spectrogram plots should be saved during feature extraction. By default, spectrograms are not saved on disk to speed up extraction.",
         default=None,
     ),
     click.option(
         "-wo",
         "--wav-out",
-        help="Convenience function to write the chunks of audio data used in the extraction to the specified folder.",
+        help=
+        "Convenience function to write the chunks of audio data used in the extraction to the specified folder.",
         default=None,
     ),
     click.option(
@@ -131,7 +148,8 @@ PLOTTING_OPTIONS = [
     click.option(
         "-fs",
         "--frequency-scale",
-        help="Scale for the y-axis of the plots used by the system. Defaults to 'chroma' in chroma mode.",
+        help=
+        "Scale for the y-axis of the plots used by the system. Defaults to 'chroma' in chroma mode.",
         default="linear",
         type=click.Choice(["linear", "log", "mel"]),
     ),
@@ -139,14 +157,16 @@ PLOTTING_OPTIONS = [
         "-d",
         "--delta",
         callback=_check_positive,
-        help="If given, derivatives of the given order of the selected features are displayed in the plots used by the system.",
+        help=
+        "If given, derivatives of the given order of the selected features are displayed in the plots used by the system.",
         default=None,
     ),
     click.option(
         "-ppdfs",
         "--pretty_pdfs",
         is_flag=True,
-        help="Add if you want to create nice pdf plots of the spectrograms the system uses. For figures in your papers ^.^",
+        help=
+        "Add if you want to create nice pdf plots of the spectrograms the system uses. For figures in your papers ^.^",
     ),
 ]
 
@@ -154,7 +174,8 @@ EXTRACTION_OPTIONS = [
     click.option(
         "-en",
         "--extraction-network",
-        help="specify the CNN that will be used for the feature extraction. You need to specify a valid weight file in .npy format in your configuration file for this network.",
+        help=
+        "specify the CNN that will be used for the feature extraction. You need to specify a valid weight file in .npy format in your configuration file for this network.",
         default="vgg16",
     ),
     click.option(
@@ -167,7 +188,8 @@ EXTRACTION_OPTIONS = [
         "-bs",
         "--batch-size",
         type=int,
-        help="Maximum batch size for feature extraction. Adjust according to your gpu memory size.",
+        help=
+        "Maximum batch size for feature extraction. Adjust according to your gpu memory size.",
         default=128,
     ),
 ]
@@ -206,7 +228,8 @@ LABEL_OPTIONS = [
     click.option(
         "-lf",
         "--label-file",
-        help="csv file with the labels for the files in the form: 'filename, label'. If nothing is specified here or under -labels, the name(s) of the directory/directories are used as labels.",
+        help=
+        "csv file with the labels for the files in the form: 'filename, label'. If nothing is specified here or under -labels, the name(s) of the directory/directories are used as labels.",
         default=None,
         type=click.Path(exists=True, dir_okay=False, readable=True),
     ),
@@ -229,44 +252,44 @@ class Configuration:
     """
 
     def __init__(
-        self,
-        plotting=True,
-        extraction=True,
-        writer=True,
-        file_types=["wav", "mp3"],
-        input=None,
-        config="deep.conf",
-        number_of_processes=max_np,
-        colour_map="viridis",
-        mode="mel",
-        frequency_scale="linear",
-        delta=None,
-        frequency_limit=None,
-        nfft=None,
-        start=0,
-        end=None,
-        window_size_and_hop=None,
-        number_of_melbands=128,
-        spectrogram_out=None,
-        wav_out=None,
-        pretty_pdfs=False,
-        extraction_network="vgg16",
-        feature_layer="fc7",
-        batch_size=128,
-        output=None,
-        time_continuous=False,
-        label_file=None,
-        explicit_label=None,
-        no_timestamps=False,
-        no_labels=False,
-        sample_rate=None,
+            self,
+            plotting=True,
+            extraction=True,
+            writer=True,
+            file_type=Filetypes.AUDIO,
+            input=None,
+            config="deep.conf",
+            number_of_processes=max_np,
+            colour_map="viridis",
+            mode="mel",
+            frequency_scale="linear",
+            delta=None,
+            frequency_limit=None,
+            nfft=None,
+            start=0,
+            end=None,
+            window_size_and_hop=None,
+            number_of_melbands=128,
+            spectrogram_out=None,
+            wav_out=None,
+            pretty_pdfs=False,
+            extraction_network="vgg16",
+            feature_layer="fc7",
+            batch_size=128,
+            output=None,
+            time_continuous=False,
+            label_file=None,
+            explicit_label=None,
+            no_timestamps=False,
+            no_labels=False,
+            sample_rate=None,
     ):
 
         self.input = input
         self.config = config
         self.number_of_processes = number_of_processes
         self.model_weights = "imagenet"
-        self.file_types = file_types
+        self.file_type = file_type
         self.plotting = plotting
         self.plotting_args = {}
         self.extraction = extraction
@@ -284,24 +307,22 @@ class Configuration:
             self.plotting_args["nfft"] = nfft
             self.plotting_args["start"] = start
             self.plotting_args["end"] = end
-            self.plotting_args["window"] = (
-                window_size_and_hop[0] if window_size_and_hop else None
-            )
-            self.plotting_args["hop"] = (
-                window_size_and_hop[1] if window_size_and_hop else None
-            )
+            self.plotting_args["window"] = (window_size_and_hop[0]
+                                            if window_size_and_hop else None)
+            self.plotting_args["hop"] = (window_size_and_hop[1]
+                                         if window_size_and_hop else None)
             self.plotting_args["resample"] = sample_rate
-            self.plotting_args["base_path"] = self.input if not isfile(self.input) else dirname(self.input)
+            self.plotting_args["base_path"] = self.input if not isfile(
+                self.input) else dirname(self.input)
             if self.plotting_args["mode"] == "mel":
                 self.plotting_args["melbands"] = number_of_melbands
             if self.plotting_args["mode"] == "chroma":
                 self.plotting_args["scale"] = "chroma"
             self.plotting_args["output_spectrograms"] = (
-                abspath(spectrogram_out) if spectrogram_out is not None else None
-            )
-            self.plotting_args["output_wavs"] = (
-                abspath(wav_out) if wav_out is not None else None
-            )
+                abspath(spectrogram_out)
+                if spectrogram_out is not None else None)
+            self.plotting_args["output_wavs"] = (abspath(wav_out) if
+                                                 wav_out is not None else None)
             if pretty_pdfs:
                 self.plotting_args["file_type"] = "pdf"
                 self.plotting_args["labelling"] = True
@@ -310,6 +331,7 @@ class Configuration:
             self.extraction_args["layer"] = feature_layer
             self.extraction_args["batch_size"] = batch_size
 
+        self._load_config()
         self.files = self._find_files(self.input)
         if not self.files:
             log.error(
@@ -319,14 +341,15 @@ class Configuration:
         if self.writer:
             self.label_file = label_file
             self.writer_args["output"] = output
-            makedirs(dirname(abspath(self.writer_args["output"])), exist_ok=True)
+            makedirs(dirname(abspath(self.writer_args["output"])),
+                     exist_ok=True)
             self.writer_args["continuous_labels"] = (
-                ("window" in self.plotting_args) and time_continuous and self.label_file
-            )
+                ("window" in self.plotting_args) and time_continuous
+                and self.label_file)
             self.writer_args["labels"] = explicit_label
             self.writer_args["write_timestamps"] = (
-                window_size_and_hop != (None, None)
-            ) and not no_timestamps and self.plotting
+                window_size_and_hop !=
+                (None, None)) and not no_timestamps and self.plotting
             self.writer_args["no_labels"] = no_labels
 
             log.info("Parsing labels...")
@@ -335,24 +358,28 @@ class Configuration:
             else:
                 self._read_label_file()
 
-        self._load_config()
-
     def _find_files(self, folder):
-        log.debug(f'Input file types are "{self.file_types}".')
-        if isfile(folder) and splitext(folder)[1][1:] in self.file_types:
-            log.debug(f"{folder} is a single {self.file_types}-file.")
+        log.debug(f'Input file types are "{self.file_type.value}".')
+        if isfile(folder) and splitext(folder)[1][1:] in self.file_type:
+            log.debug(f"{folder} is a single {self.file_type.value}-file.")
             return [folder]
 
         input_files = []
-        for file_type in self.file_types:
+        for file_type in self.file_type.value:
             globexpression = "*." + file_type
-            reg_expr = re.compile(fnmatch.translate(globexpression), re.IGNORECASE)
+            reg_expr = re.compile(fnmatch.translate(globexpression),
+                                  re.IGNORECASE)
             log.debug(f"Searching {folder} for {file_type}-files.")
             for root, dirs, files in walk(folder, topdown=True):
-                new_files = [join(root, j) for j in files if re.match(reg_expr, j)]
-                log.debug(f"Found {len(new_files)} {file_type}-files in {root}.")
+                new_files = [
+                    join(root, j) for j in files if re.match(reg_expr, j)
+                ]
+                log.debug(
+                    f"Found {len(new_files)} {file_type}-files in {root}.")
                 input_files += new_files
-        log.debug(f"Found a total of {len(input_files)} {self.file_types}-files.")
+        log.debug(
+            f"Found a total of {len(input_files)} {self.file_type.value}-files."
+        )
         return input_files
 
     def _read_label_file(self):
@@ -387,8 +414,7 @@ class Configuration:
                 f"No labels for: {len(missing_labels)} files. Only processing files with labels."
             )
             self.files = [
-                file
-                for file in self.files
+                file for file in self.files
                 if basename(file) in self.writer_args["label_dict"]
             ]
 
@@ -399,19 +425,19 @@ class Configuration:
         """
         if self.writer_args["labels"] is None:
             self.writer_args["label_dict"] = {
-                basename(wav): [basename(dirname(wav))] for wav in self.files
+                basename(wav): [basename(dirname(wav))]
+                for wav in self.files
             }
         else:
             # map the labels given on the commandline to all files in a given folder to all input files
             self.writer_args["label_dict"] = {
-                basename(wav): [str(self.writer_args["labels"])] for wav in self.files
+                basename(wav): [str(self.writer_args["labels"])]
+                for wav in self.files
             }
         labels = sorted(
-            list(map(lambda x: x[0], self.writer_args["label_dict"].values()))
-        )
+            list(map(lambda x: x[0], self.writer_args["label_dict"].values())))
 
         self.writer_args["labels"] = [("class", set(labels))]
-
 
     def _load_config(self):
         """
@@ -429,23 +455,23 @@ class Configuration:
             main_conf = conf_parser["main"]
             self.plotting_args["size"] = int(main_conf["size"])
             self.backend = main_conf["backend"]
+            filetypes = Enum(
+                'ConfigurationFiletypes', {
+                    'AUDIO': main_conf['audioFormats'].split(','),
+                    'IMAGE': main_conf['imageFormats'].split(',')
+                })
+            self.file_type = filetypes[self.file_type.name]
             if self.extraction:
 
                 if self.backend == "keras":
-                    log.info(
-                        "Using keras backend as specified in {}".format(self.config)
-                    )
-                    try:
-                        import tensorflow
-                        import tensorflow.keras
-
-                        self.extractor = KerasExtractor
-                    except ImportError:
-                        log.exception("No keras installation found!")
+                    log.info("Using keras backend as specified in {}".format(
+                        self.config))
+                    self.extractor = KerasExtractor
 
                     net_conf = conf_parser["keras-nets"]
                     if self.net in net_conf:
-                        self.extraction_args["weights_path"] = net_conf[self.net]
+                        self.extraction_args["weights_path"] = net_conf[
+                            self.net]
                         self.extraction_args["model_key"] = self.net
                     else:
                         log.error(
@@ -461,13 +487,13 @@ class Configuration:
         # if not, create it with standard settings
         else:
             log.info("Writing standard config to " + self.config)
-            main_conf = {"size": str(227), "backend": "keras"}
-
-            keras_net_conf = {"vgg16": "imagenet"}
-
-            conf_parser["main"] = main_conf
-            conf_parser["keras-nets"] = keras_net_conf
+            
+            makedirs(dirname(abspath(self.config)), exist_ok=True)
+            # Read the defaul config file included in the package
+            conf_parser.read(join(dirname(realpath(__file__)), "deep.conf"))
             with open(self.config, "w") as configfile:
                 conf_parser.write(configfile)
-                log.error(f"Please initialize your configuration file in {self.config}")
+                log.error(
+                    f"Please initialize your configuration file in {self.config}"
+                )
                 exit(1)
--- tests/cli/test_plot.py
+++ tests/cli/test_plot.py
@@ -2,25 +2,23 @@ from click.testing import CliRunner
 from deepspectrum.__main__ import cli
 from multiprocessing import cpu_count
 from os.path import join, dirname
-from shutil import rmtree
 
 cur_dir = dirname(__file__)
 examples = join(dirname(dirname(cur_dir)), 'examples')
 
 
-def test_plot():
-    rmtree('/tmp/deepspectrumtest', ignore_errors=True)
+def test_plot(tmpdir):
     runner = CliRunner()
     result = runner.invoke(cli,
                            args=[
                                '-v', 'plot',
                                join(examples, 'audio'), '-np',
                                cpu_count(), '-cm', 'twilight', '-so',
-                               '/tmp/deepspectrumtest/pretty-spectrograms',
-                               '-sr', 16000, '-m', 'mel', '-fs', 'spectrogram',
-                               '-fs', 'log', '-ppdfs', '-d', '1', '-wo',
-                               '/tmp/deepspectrumtest/wav-chunks', '-t', '1',
-                               '1', '-fql', '12000'
+                               join(tmpdir, 'pretty-spectrograms'), '-sr',
+                               16000, '-m', 'mel', '-fs', 'spectrogram', '-fs',
+                               'log', '-ppdfs', '-d', '1', '-wo',
+                               join(tmpdir, 'wav-chunks'), '-t', '1', '1',
+                               '-fql', '12000'
                            ])
     assert 'Done' in result.output
     assert result.exit_code == 0
